a431a2a89089c71bd6c77b48d3ad1f165b1f1198
Tarran Wylie <124862204+TarranW09@users.noreply.github.com>
2023-08-08 19:25:53 -0600

    Add files via upload

---
+++ Controllers/InsureeController.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CarInsurance.Models;
+
+namespace CarInsurance.Controllers
+{
+    public class InsureeController : Controller
+    {
+        private InsuranceEntities db = new InsuranceEntities();
+
+        // GET: Insuree
+        public ActionResult Index()
+        {
+            return View(db.Tables.ToList());
+        }
+
+        // GET: Insuree/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Tables.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        // GET: Insuree/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Insuree/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Table table)
+        {
+            if (ModelState.IsValid)
+            {
+                // Calculate the base quote
+                decimal baseQuote = 50;
+
+                // Calculate the age based on the DateOfBirth
+                int age = CalculateAge(table.DateOfBirth);
+
+                // Adjust the quote based on age
+                if (age <= 18)
+                {
+                    baseQuote += 100;
+                }
+                else if (age >= 19 && age <= 25)
+                {
+                    baseQuote += 50;
+                }
+                else
+                {
+                    baseQuote += 25;
+                }
+
+                // Adjust the quote based on car year
+                if (table.CarYear < 2000)
+                {
+                    baseQuote += 25;
+                }
+                else if (table.CarYear > 2015)
+                {
+                    baseQuote += 25;
+                }
+
+                // Adjust the quote based on car make and model
+                if (table.CarMake.ToLower() == "porsche")
+                {
+                    baseQuote += 25;
+                    if (table.CarModel.ToLower() == "911 carrera")
+                    {
+                        baseQuote += 25;
+                    }
+                }
+
+                // Adjust the quote based on speeding tickets
+                baseQuote += table.SpeedingTickets * 10;
+
+                // Adjust the quote based on DUI
+                if (table.DUI)
+                {
+                    baseQuote += baseQuote * 0.25m;
+                }
+                //Adjust the quote based on coverage type
+                if (table.CoverageType)
+                {
+                    baseQuote += baseQuote * (decimal)0.5;
+                }
+
+
+                // Set the calculated quote to the Quote property of the table object
+                table.Quote = baseQuote;
+
+                db.Tables.Add(table);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(table);
+        }
+
+        private int CalculateAge(DateTime dateOfBirth)
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - dateOfBirth.Year;
+            if (dateOfBirth.Date > today.AddYears(-age))
+            {
+                age--;
+            }
+            return age;
+        }
+
+        // GET: Insuree/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Tables.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        // POST: Insuree/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Table table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(table).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(table);
+        }
+
+        // GET: Insuree/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Tables.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        // POST: Insuree/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Table table = db.Tables.Find(id);
+            db.Tables.Remove(table);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        // GET: Insuree/AdminIndex
+        public ActionResult AdminIndex()
+        {
+            var insurees = db.Tables.ToList();
+            return View(insurees);
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
